5f3352d48320b978a8629ec2d3e23868efc0dba8
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-08 18:26:42 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,9 +3,6 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   
   def say_that_thing_you_say 
-    characters.collect do |character|
-      "#{character.catchphrase}"
-    end 
+    "#{self.name} says #{self.catchphrase}"
   end 
-  
 end
