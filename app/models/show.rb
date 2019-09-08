1f5ad4736656b2def165139b642abf3900b3b48d
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-08 18:39:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through :characters
   
   def actors_list
-    self.actors.map do |actor|
+    self.actors.collect do |actor|
       actor.full_name
     end 
   end 
